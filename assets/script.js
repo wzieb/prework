db3eb0b878aef4c3c43ef39448b9a37eeb0fb370
wziebe <w.ziebert@gmail.com>
2024-06-23 11:38:59 -0500

    more js features

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,26 @@
-const topic = [ 'HTML','CSS','Git','Javascript'];
-for (x = 0; x<topic.length; x++) {
-  console.log(topic[x]);
-} 
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-
-
-const shapes = ["triangle","square","pentagon","circle"];
-for (let x = 0; x <shapes.length; x++) {
-  console.log(shapes[x]); 
-
+function listTopics(){
+  for (let x=0; x<topics.length;x++) {
+    console.log(topics[x]);
+  }
 }
 
+function selectTopic() {
+  if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
