84f766cdff64bcc22d621e2d0674de36b6dcd291
wziebe <w.ziebert@gmail.com>
2024-06-23 11:12:58 -0500

    added JS features

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+const topic = [ 'HTML','CSS','Git','Javascript'];
+for (x = 0; x<topic.length; x++) {
+  console.log(topic[x]);
+} 
+
+
+
+const shapes = ["triangle","square","pentagon","circle"];
+for (let x = 0; x <shapes.length; x++) {
+  console.log(shapes[x]); 
+
+}
+
